99a68bf106275f08a0d8305382ebac8aa4d8b965
ForNein <ForNein@users.noreply.github.com>
2014-03-06 20:05:55 -0500

    Validation
    Validate if input is roman numeral

--- src/test/spec/mainSpec.js
+++ src/test/spec/mainSpec.js
@@ -8,4 +8,38 @@ describe("Roman Numeral Calculator", function() {
         expect(values.D).toEqual(500);
         expect(values.M).toEqual(1000);
     });
+
+    it("roman numeral validator returns true for valid roman numerals", function() {
+        expect(isRomanNumeral('I')).toBe(true);
+        expect(isRomanNumeral('II')).toBe(true);
+        expect(isRomanNumeral('III')).toBe(true);
+        expect(isRomanNumeral('IV')).toBe(true);
+        expect(isRomanNumeral('V')).toBe(true);
+        expect(isRomanNumeral('VI')).toBe(true);
+        expect(isRomanNumeral('VII')).toBe(true);
+        expect(isRomanNumeral('VIII')).toBe(true);
+        expect(isRomanNumeral('IX')).toBe(true);
+        expect(isRomanNumeral('X')).toBe(true);
+        expect(isRomanNumeral('XX')).toBe(true);
+        expect(isRomanNumeral('XXX')).toBe(true);
+        expect(isRomanNumeral('XL')).toBe(true);
+        expect(isRomanNumeral('L')).toBe(true);
+        expect(isRomanNumeral('LX')).toBe(true);
+        expect(isRomanNumeral('LXX')).toBe(true);
+        expect(isRomanNumeral('LXXX')).toBe(true);
+        expect(isRomanNumeral('XC')).toBe(true);
+        expect(isRomanNumeral('C')).toBe(true);
+        expect(isRomanNumeral('CC')).toBe(true);
+        expect(isRomanNumeral('CCC')).toBe(true);
+        expect(isRomanNumeral('CD')).toBe(true);
+        expect(isRomanNumeral('D')).toBe(true);
+        expect(isRomanNumeral('DC')).toBe(true);
+        expect(isRomanNumeral('DCC')).toBe(true);
+        expect(isRomanNumeral('DCCC')).toBe(true);
+        expect(isRomanNumeral('CM')).toBe(true);
+        expect(isRomanNumeral('M')).toBe(true);
+        expect(isRomanNumeral('MM')).toBe(true);
+        expect(isRomanNumeral('MMM')).toBe(true);
+        expect(isRomanNumeral('MMMM')).toBe(true);
+    });
 });
